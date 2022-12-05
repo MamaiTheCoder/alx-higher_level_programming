5b539166ab701a5020010c166902dff8f87ff560
MamaiTheCoder <jmakoks@gmail.com>
2022-12-05 03:54:53 -0800

    Update

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    """Replace an element in a copied list at a specific position."""
     length = len(my_list)
-    new_list = my_list[:]
 
-    if 0 < idx < length:
-        new_list[idx] = element
+	if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
 
+    new_list = [x  for x in my_list]
+    new_list[idx] = element
     return (new_list)
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,3 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_string = my_string[:]
-    j = 0
-    for c in range(len(my_string)):
-        if my_string[c] == 'c' or my_string[c] == 'C':
-            new_string = new_string[:(c - j)] + my_string[(c + 1):]
-            j += 1
-    return (new_string)
+     return ("".join(c for c in my_string if c not in "Cc"))
