819e21d62f37b82d0fa1b88d32bb12131630f80e
MamaiTheCoder <jmakoks@gmail.com>
2023-03-14 07:02:08 -0700

    Delete files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-// Concats 2 files
-const fs = require('fs');
-
-const fArg = fs.readFileSync(process.argv[2].toString());
-const sArg = fs.readFileSync(process.argv[3].toString());
-
-fs.writeFileSync(process.argv[4], `${fArg}\n${sArg}`);
