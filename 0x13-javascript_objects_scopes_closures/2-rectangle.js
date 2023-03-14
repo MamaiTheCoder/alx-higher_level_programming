68a99a699a8d847ad2770fbe0b0794b6c48f89c7
MamaiTheCoder <jmakoks@gmail.com>
2023-03-14 15:01:39 +0300

    Rename class

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // A class Rectangle that defines a rectangle.
-module.exports = class Rectagle {
+class Rectangle {
   constructor (w, h) {
     // If w or h is equal to 0 or not a positive integer, create an empty object.
     if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
@@ -9,3 +9,5 @@ module.exports = class Rectagle {
     }
   }
 }
+
+module.exports = Rectangle;
