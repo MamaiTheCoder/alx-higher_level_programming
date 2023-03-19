d68d4a0a910998184be76c19c8864a5d1e706fb8
MamaiTheCoder <jmakoks@gmail.com>
2023-03-19 00:45:16 -0700

    Create Object relational mapping dir

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Lists all states from database hbtn_0e_0_usa.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
