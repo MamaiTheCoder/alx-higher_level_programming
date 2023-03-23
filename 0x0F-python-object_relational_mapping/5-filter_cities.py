ddb1d9c3a878b119aa82cea8db9a64d454da0f3c
Emmanuel Makokha <99004575+MamaiTheCoder@users.noreply.github.com>
2023-03-23 14:33:00 +0300

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,6 +8,43 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
+    # Check if 4 arguments are provided
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database_name state_name".format(sys.argv[0]))
+        sys.exit(1)
+    
+    # Get arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to database
+    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database_name)
+
+    # Create cursor
+    cursor = db.cursor()
+
+    # Execute query
+    cursor.execute("SELECT cities.id, cities.name, states.name 
+                    FROM cities 
+                    JOIN states ON cities.state_id = states.id 
+                    WHERE states.name LIKE %s 
+                    ORDER BY cities.id ASC", (state_name,))
+
+    # Get results
+    results = cursor.fetchall()
+
+    # Display results
+    for row in results:
+        print("{}: ({}) {}".format(row[2], row[0], row[1]))
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
+
+    
+    
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
 
@@ -20,3 +57,5 @@ if __name__ == '__main__':
     states = cur.fetchall()
 
     print(", ".join([state[1] for state in states]))
+                
+                
