768424732107121503b1f47d4f7ae87acbef1f5d
MamaiTheCoder <jmakoks@gmail.com>
2023-03-30 08:05:42 -0700

    Make one line of code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/bash
-# Takes in a URL, sends a GET request to the URL, and displays the body of the response.
-# Check if a URL was provided as an argument
-
+#!/bin/bash
+# Takes in a URL, sends a GET request to the URL, and displays the body of the response.Check if a URL was provided as an argument
 curl -Ls "$1"
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Sends a DELETE request to the URL passed as the first argument and displays the body of the response.
 curl -s "$1" -X DELETE
