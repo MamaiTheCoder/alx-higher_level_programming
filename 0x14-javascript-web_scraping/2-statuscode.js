9eebfff0033b32aa07dd08585ccf678191a06b52
MamaiTheCoder <jmakoks@gmail.com>
2023-05-22 19:12:42 -0700

    Add if condition

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,8 @@
 
 // Display the status code of a GET request.
 const request = require('request');
-
-request(process.argv[2], function (_err, res) {
-  console.log('Code: ', res.statusCode);
+request(process.argv[2], function (error, response) {
+  if (error == null) {
+    console.log('code: ' + response.statusCode);
+  }
 });
